ba26e2ca7ce2348ebed3189f30819500f5267b4b
sister_clown <e115731@sister.local>
2018-11-01 01:59:35 +0900

    取得したデータの成形まで

--- src/main/java/com/ddr/matching/Scraping.java
+++ src/main/java/com/ddr/matching/Scraping.java
@@ -1,62 +1,42 @@
 package com.ddr.matching;
 
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.http.HttpStatus;
-import org.xml.sax.InputSource;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Scraping {
     static String cookie = "";
     static String targetUrl = "https://p.eagate.573.jp/game/ddr/ddra/p/playdata/music_recent.html";
 
     public static void main(String args[]) {
-        String ua = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36";
-        String password = "test";
         try {
-
-            Connection.Response res1 = Jsoup.connect(targetUrl).method(Connection.Method.GET).execute();
-            Document welcomePage = res1.parse();
-            Map welcomCookies = res1.cookies();
-            welcomCookies.put("M573SSID", cookie);
-            Element inputHidden = welcomePage.getElementById("csrftoken");
-
-            Connection.Response res2 = Jsoup.connect(targetUrl).header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8").cookies(welcomCookies).method(Connection.Method.POST).execute();
+            Map<String, String> cookies = new TreeMap<>();
+            cookies.put("M573SSID", cookie);
+            Connection.Response res2 = Jsoup.connect(targetUrl).header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8").cookies(cookies).method(Connection.Method.GET).execute();
             Document document = res2.parse();
+            System.out.println(document);
             Element body = document.body();
-            Elements ddrBodies = body.getElementsByAttributeValue("id", "ddr_body");
-            Element ddrBody = ddrBodies.first();
-            Elements trest = ddrBody.getElementsByTag("tbody");
-            List<Element> nodes = trest.first().children().stream().filter(node -> node.getElementsByTag("tr").size() != 0).map(node -> {
-                System.out.println(node);
-                return node;
+            Elements tables = body.getElementsByTag("tbody").first().children();
+            tables.remove(0);
+            List<String> info = tables.stream().map(node -> {
+                Elements children = node.children();
+                String musicTitle = children.get(0).text();
+                String diff = children.get(1).text();
+                String score = children.get(3).text();
+                return musicTitle + "(" + diff + ") : " +  score;
             }).collect(Collectors.toList());
+            info.forEach(System.out::println);
             } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private static org.w3c.dom.Document convertStringToDocument(String carProfileXml) {
-        try {
-            InputSource inputSource = new InputSource(new StringReader(carProfileXml));
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            org.w3c.dom.Document document = builder.parse(inputSource);
-            org.w3c.dom.Element element = document.getDocumentElement();
-            return document;
-        } catch (Exception e) {
-            return null;
         }
     }
 }
